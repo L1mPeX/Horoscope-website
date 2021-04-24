5360ad3d7b5205ff9b4264e2634cfe3e8112c877
Andrei Kazarin <61632926+L1mPeX@users.noreply.github.com>
2021-04-24 10:05:57 +0300

    Create server.py
    The local server startup is placed in a separate file

---
+++ server.py
@@ -0,0 +1,24 @@
+from bottle import route, run, view
+from datetime import datetime as dt
+from random import randint
+from horoscope import generate_prophecies
+
+
+@route("/")
+@view("predictions")
+def index():
+    now = dt.now()
+    x = randint(0, 4)
+    l = generate_prophecies()
+    print(l)
+    return {
+        "date": f"{now.year}-{now.month}-{now.day}",
+        "predictions": l[x],
+        "x": x
+    }
+
+run(
+    host="localhost",
+    port=8080,
+    autoreload=True
+)
